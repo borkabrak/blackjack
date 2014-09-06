8f1816012f94248659a3e46301d23aeaf24b157b
Jonathan Carter <borkabrak@gmail.com>
2014-09-06 03:57:04 -0500

    Show the cards
    Start with a complete deck of cards displayed, spread out nicely across
    the table.

--- blackjack.js
+++ blackjack.js
@@ -3,6 +3,63 @@ var Card = function(rank, suit){
   this.rank = rank;
   this.suit = suit;
 
+  var colors = {
+    "Spades": "black",
+    "Hearts": "red",
+    "Diamonds": "red",
+    "Clubs": "black"
+  };
+
+  this.color = colors[suit];
+
+  var symbols = {
+
+    suits: {
+      "Spades": "&spades;",
+      "Hearts": "&hearts;",
+      "Diamonds": "&diams;",
+      "Clubs": "&clubs;"
+    },
+
+    ranks: {
+      1: "A",
+      2: "2",
+      3: "3",
+      4: "4",
+      5: "5",
+      6: "6",
+      7: "7",
+      8: "8",
+      9: "9",
+      10: "10",
+      11: "J",
+      12: "Q",
+      13: "K"
+    }
+  };
+
+
+  function createLabel(classname){
+    var label = document.createElement("span");
+    label.innerHTML = symbols.ranks[rank] + " " + symbols.suits[suit];
+    label.setAttribute("class", classname);
+    return label;
+  };
+
+  // Create the HTML structure
+  this.html = document.createElement("div");
+  this.html.setAttribute("class", "card " + this.color);
+
+  // labels (rank and suit signifiers at top left and bottom right)
+  var labeltext = this.rank + this.suit;
+  var label1 = createLabel("label1");
+  var label2 = createLabel("label2");
+  var label3 = createLabel("label3");
+
+  this.html.appendChild(label1);
+  this.html.appendChild(label2);
+  this.html.appendChild(label3);
+
 };
 
 var Deck = function(){
@@ -12,10 +69,19 @@ var Deck = function(){
   this.ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
   this.suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 
-  for (rank in this.ranks){
-    for (suit in this.suits){
+  for (suit in this.suits){
+    for (rank in this.ranks){
       this.cards.push(new Card(this.ranks[rank], this.suits[suit]));
     }
   }
+
+  this.show = function(container_selector){
+    var container = document.querySelector(container_selector);
+    for(i in this.cards){
+      var card = this.cards[i];
+      card.html.style.left = (i * 10) + "px";
+      container.appendChild(card.html);
+    };
+  };
   
 };
